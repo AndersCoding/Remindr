ad15405256b0521193f9a7ffe0e2225323f87414
AndersB <Andersbbeil@gmail.com>
2024-10-02 10:50:27 +0200

    Initial Commit

---
+++ Remindr/RemindrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindrApp.swift
+//  Remindr
+//
+//  Created by Anders Bellsund Beil on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RemindrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RemindrTests/RemindrTests.swift
@@ -0,0 +1,17 @@
+//
+//  RemindrTests.swift
+//  RemindrTests
+//
+//  Created by Anders Bellsund Beil on 02/10/2024.
+//
+
+import Testing
+@testable import Remindr
+
+struct RemindrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
